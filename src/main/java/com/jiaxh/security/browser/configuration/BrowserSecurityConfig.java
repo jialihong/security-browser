7b3b8080e60efcedfca9deeec1e0aaecd1288785
jiaxh <jiaxh2018>
2019-05-23 23:04:48 +0800

    记住我

--- src/main/java/com/jiaxh/security/browser/configuration/BrowserSecurityConfig.java
+++ src/main/java/com/jiaxh/security/browser/configuration/BrowserSecurityConfig.java
@@ -8,11 +8,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+
+import javax.sql.DataSource;
 
 /**
  * WebSecurityConfigurerAdapter 是Spring Security提供的一个安全适配器类
@@ -37,23 +42,38 @@ public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private AuthenticationFailureHandler jiaAuthenticationFailureHandler;
 
+    @Autowired
+    private DataSource dataSource;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    public PersistentTokenRepository persistentTokenRepository(){
+        JdbcTokenRepositoryImpl tokenRepository = new JdbcTokenRepositoryImpl();
+        tokenRepository.setDataSource(dataSource);
+        //系统启动时自动创建表
+        tokenRepository.setCreateTableOnStartup(true);
+        return tokenRepository;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-//        ValidateCodeFilter validateCodeFilter = new ValidateCodeFilter();
-//        validateCodeFilter.setAuthenticationFailureHandler(jiaAuthenticationFailureHandler);
-//        validateCodeFilter.setSecurityProperties(securityProperties);
-//        validateCodeFilter.afterPropertiesSet();
+        ValidateCodeFilter validateCodeFilter = new ValidateCodeFilter();
+        validateCodeFilter.setAuthenticationFailureHandler(jiaAuthenticationFailureHandler);
+        validateCodeFilter.setSecurityProperties(securityProperties);
+        validateCodeFilter.afterPropertiesSet();
 
         //使用原生的http请求
         //http.httpBasic()
         http
                 //将自定义的验证码校验的过滤器设置到UsernamePasswordAuthenticationFilter之前
-//                .addFilterBefore(validateCodeFilter, UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(validateCodeFilter, UsernamePasswordAuthenticationFilter.class)
                 .formLogin()
                 //使用自定义的登录页面的url，如果不加该方法则使用security默认的登录页
 //                .loginPage("/jia-login.html")
@@ -64,6 +84,12 @@ public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
                 .successHandler(jiaAuthenticationSuccessHandler)
 //                //使用自定义的失败处理器
                 .failureHandler(jiaAuthenticationFailureHandler)
+                .and()
+                //记住我
+                .rememberMe()
+                .tokenRepository(persistentTokenRepository())
+                .tokenValiditySeconds(securityProperties.getBrowser().getRememberMeSeconds())
+                .userDetailsService(userDetailsService)
                 .and()
                 .authorizeRequests()
                 //访问以下url时不需要认证，否则会报 重定向次数过多 的错误
