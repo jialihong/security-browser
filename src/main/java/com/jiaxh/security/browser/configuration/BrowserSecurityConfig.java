5e8bca16ea891baf6c3dfabbdf205168547bfae3
jiaxh <jia_xiaohong@xiniaoyun.com>
2019-06-11 15:50:17 +0800

    短信验证码登录

--- src/main/java/com/jiaxh/security/browser/configuration/BrowserSecurityConfig.java
+++ src/main/java/com/jiaxh/security/browser/configuration/BrowserSecurityConfig.java
@@ -1,7 +1,9 @@
 package com.jiaxh.security.browser.configuration;
 
+import com.jiaxh.security.core.authentication.mobile.SmsCodeAuthenticationSecurityConfig;
 import com.jiaxh.security.core.properties.SecurityProperties;
 import com.jiaxh.security.core.validate.code.ValidateCodeFilter;
+import com.jiaxh.security.core.validate.code.sms.SmsCodeFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -48,6 +50,9 @@ public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private UserDetailsService userDetailsService;
 
+    @Autowired
+    private SmsCodeAuthenticationSecurityConfig smsCodeAuthenticationSecurityConfig;
+
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
@@ -64,15 +69,24 @@ public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        //配置图形验证码校验的filter
         ValidateCodeFilter validateCodeFilter = new ValidateCodeFilter();
         validateCodeFilter.setAuthenticationFailureHandler(jiaAuthenticationFailureHandler);
         validateCodeFilter.setSecurityProperties(securityProperties);
         validateCodeFilter.afterPropertiesSet();
 
+        //配置短信验证码校验的filter
+        SmsCodeFilter smsCodeFilter = new SmsCodeFilter();
+        smsCodeFilter.setAuthenticationFailureHandler(jiaAuthenticationFailureHandler);
+        smsCodeFilter.setSecurityProperties(securityProperties);
+        smsCodeFilter.afterPropertiesSet();
+
         //使用原生的http请求
         //http.httpBasic()
         http
-                //将自定义的验证码校验的过滤器设置到UsernamePasswordAuthenticationFilter之前
+                //将自定义的短信验证码校验的过滤器设置到UsernamePasswordAuthenticationFilter之前
+                .addFilterBefore(smsCodeFilter,UsernamePasswordAuthenticationFilter.class)
+                //将自定义的图形验证码校验的过滤器设置到UsernamePasswordAuthenticationFilter之前
                 .addFilterBefore(validateCodeFilter, UsernamePasswordAuthenticationFilter.class)
                 .formLogin()
                 //使用自定义的登录页面的url，如果不加该方法则使用security默认的登录页
@@ -93,12 +107,14 @@ public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .authorizeRequests()
                 //访问以下url时不需要认证，否则会报 重定向次数过多 的错误
-                .antMatchers("/authentication/require",securityProperties.getBrowser().getLoginPage(),"/code/image")
+                .antMatchers("/authentication/require",securityProperties.getBrowser().getLoginPage(),"/code/image","/code/*")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
         .and()
          //关闭csrf验证 跨站请求伪造防护
-        .csrf().disable();
+        .csrf().disable()
+        //使用SmsCodeAuthenticationSecurityConfig
+        .apply(smsCodeAuthenticationSecurityConfig);
     }
 }
