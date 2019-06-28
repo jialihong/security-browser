064a0a805f371b9e54fba729f31fc0127a2fc414
jiaxh <jia_xiaohong@xiniaoyun.com>
2019-06-28 14:38:01 +0800

    代码重构成功

---
+++ src/main/java/com/jiaxh/security/browser/configuration/AbstractChannelSecurityConfig.java
@@ -0,0 +1,29 @@
+package com.jiaxh.security.browser.configuration;
+
+import com.jiaxh.security.core.properties.SecurityConstants;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+/**
+ * @Auther: jiaxh
+ * @Date: 2019/6/27 17:05
+ */
+public class AbstractChannelSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    protected AuthenticationSuccessHandler jiaAuthenticationSuccessHandler;
+
+    @Autowired
+    protected AuthenticationFailureHandler jiaAuthenticationFailureHandler;
+
+    protected void applyPasswordAuthenticationConfig(HttpSecurity http) throws Exception {
+        http.formLogin()
+                .loginPage(SecurityConstants.DEFAULT_UNAUTHENTICATION_URL)
+                .loginProcessingUrl(SecurityConstants.DEFAULT_LOGIN_PROCESSING_URL_FORM)
+                .successHandler(jiaAuthenticationSuccessHandler)
+                .failureHandler(jiaAuthenticationFailureHandler);
+    }
+}
--- src/main/java/com/jiaxh/security/browser/configuration/BrowserSecurityConfig.java
+++ src/main/java/com/jiaxh/security/browser/configuration/BrowserSecurityConfig.java
@@ -1,24 +1,18 @@
 package com.jiaxh.security.browser.configuration;
 
 import com.jiaxh.security.core.authentication.mobile.SmsCodeAuthenticationSecurityConfig;
+import com.jiaxh.security.core.properties.SecurityConstants;
 import com.jiaxh.security.core.properties.SecurityProperties;
-import com.jiaxh.security.core.validate.code.ValidateCodeFilter;
-import com.jiaxh.security.core.validate.code.sms.SmsCodeFilter;
+import com.jiaxh.security.core.validate.code.ValidateCodeSecurityConfig;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
 import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
-
 import javax.sql.DataSource;
 
 /**
@@ -27,22 +21,10 @@ import javax.sql.DataSource;
  * @Date: 2019/5/6 10:42
  */
 @Configuration
-public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
+public class BrowserSecurityConfig extends AbstractChannelSecurityConfig {
 
     @Autowired
     private SecurityProperties securityProperties;
-
-    /**
-     * 自定义登录成功处理器
-     */
-    @Autowired
-    private AuthenticationSuccessHandler jiaAuthenticationSuccessHandler;
-
-    /**
-     * 自定义登录失败处理器
-     */
-    @Autowired
-    private AuthenticationFailureHandler jiaAuthenticationFailureHandler;
 
     @Autowired
     private DataSource dataSource;
@@ -52,6 +34,9 @@ public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private SmsCodeAuthenticationSecurityConfig smsCodeAuthenticationSecurityConfig;
+
+    @Autowired
+    private ValidateCodeSecurityConfig validateCodeSecurityConfig;
 
     @Bean
     public PasswordEncoder passwordEncoder(){
@@ -69,52 +54,28 @@ public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        //配置图形验证码校验的filter
-        ValidateCodeFilter validateCodeFilter = new ValidateCodeFilter();
-        validateCodeFilter.setAuthenticationFailureHandler(jiaAuthenticationFailureHandler);
-        validateCodeFilter.setSecurityProperties(securityProperties);
-        validateCodeFilter.afterPropertiesSet();
+        applyPasswordAuthenticationConfig(http);
 
-        //配置短信验证码校验的filter
-        SmsCodeFilter smsCodeFilter = new SmsCodeFilter();
-        smsCodeFilter.setAuthenticationFailureHandler(jiaAuthenticationFailureHandler);
-        smsCodeFilter.setSecurityProperties(securityProperties);
-        smsCodeFilter.afterPropertiesSet();
-
-        //使用原生的http请求
-        //http.httpBasic()
-        http
-                //将自定义的短信验证码校验的过滤器设置到UsernamePasswordAuthenticationFilter之前
-                .addFilterBefore(smsCodeFilter,UsernamePasswordAuthenticationFilter.class)
-                //将自定义的图形验证码校验的过滤器设置到UsernamePasswordAuthenticationFilter之前
-                .addFilterBefore(validateCodeFilter, UsernamePasswordAuthenticationFilter.class)
-                .formLogin()
-                //使用自定义的登录页面的url，如果不加该方法则使用security默认的登录页
-//                .loginPage("/jia-login.html")
-                .loginPage("/authentication/require")
-                //登录页提交时的url，向该url提交时，使用UsernamePasswordAuthenticationFilter验证用户名密码是否正确
-                .loginProcessingUrl("/authentication/form")
-                //使用自定义的成功处理器
-                .successHandler(jiaAuthenticationSuccessHandler)
-//                //使用自定义的失败处理器
-                .failureHandler(jiaAuthenticationFailureHandler)
+        http.apply(validateCodeSecurityConfig)
                 .and()
-                //记住我
+                .apply(smsCodeAuthenticationSecurityConfig)
+                .and()
                 .rememberMe()
-                .tokenRepository(persistentTokenRepository())
-                .tokenValiditySeconds(securityProperties.getBrowser().getRememberMeSeconds())
-                .userDetailsService(userDetailsService)
-                .and()
+                    .tokenRepository(persistentTokenRepository())
+                    .tokenValiditySeconds(securityProperties.getBrowser().getRememberMeSeconds())
+                    .userDetailsService(userDetailsService)
+                    .and()
                 .authorizeRequests()
-                //访问以下url时不需要认证，否则会报 重定向次数过多 的错误
-                .antMatchers("/authentication/require",securityProperties.getBrowser().getLoginPage(),"/code/image","/code/*")
+                .antMatchers(
+                        SecurityConstants.DEFAULT_UNAUTHENTICATION_URL,
+                        SecurityConstants.DEFAULT_LOGIN_PROCESSING_URL_MOBILE,
+                        securityProperties.getBrowser().getLoginPage(),
+                        SecurityConstants.DEFAULT_VALIDATE_CODE_URL_PREFIX+"/*")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
-        .and()
-         //关闭csrf验证 跨站请求伪造防护
-        .csrf().disable()
-        //使用SmsCodeAuthenticationSecurityConfig
-        .apply(smsCodeAuthenticationSecurityConfig);
+                .and()
+                .csrf().disable();
+
     }
 }
